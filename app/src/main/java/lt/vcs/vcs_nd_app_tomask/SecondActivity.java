b75d7bf5ea7ae6fd7f60a9c9344d36f560068661
Tomas <tomas.kubilius.93@gmail.com>
2022-01-21 13:56:20 +0200

    Add @override to all SecondActivity lifecycle

--- app/src/main/java/lt/vcs/vcs_nd_app_tomask/SecondActivity.java
+++ app/src/main/java/lt/vcs/vcs_nd_app_tomask/SecondActivity.java
@@ -1,6 +1,46 @@
 package lt.vcs.vcs_nd_app_tomask;
 
-public class SecondActivity {
 
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
 
 }
