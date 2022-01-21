cf916a1f7472c970aee1b893c7337e1103849411
Tomas <tomas.kubilius.93@gmail.com>
2022-01-21 12:00:46 +0200

    Initial commit

---
+++ app/src/main/java/lt/vcs/vcs_nd_app_tomask/MainActivity.java
@@ -0,0 +1,14 @@
+package lt.vcs.vcs_nd_app_tomask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
