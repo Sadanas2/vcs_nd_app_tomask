54d6b832fed1157195cbc7768f3b0f0f7e53c5e9
Tomas <tomas.kubilius.93@gmail.com>
2022-01-21 13:59:44 +0200

    create&add printStatus in SecondActivity lifecycle

--- app/src/main/java/lt/vcs/vcs_nd_app_tomask/SecondActivity.java
+++ app/src/main/java/lt/vcs/vcs_nd_app_tomask/SecondActivity.java
@@ -2,45 +2,61 @@ package lt.vcs.vcs_nd_app_tomask;
 
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class SecondActivity extends AppCompatActivity {
 
+    public final static String LOG_TAG= "log_second_activity";
+
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        printStatus("~~~~~~~~~~~");
+        printStatus(": onCreate");
+    }
+    private void printStatus(String statusName){
+        Log.i(LOG_TAG,"first activity: "+statusName);
     }
 
     @Override
     protected void onStart(){
         super.onStart();
+        printStatus("~~~~~~~~~~~");
+        printStatus(": onStart");
     }
 
     @Override
     protected void onResume(){
         super.onResume();
+        printStatus("~~~~~~~~~~~");
+        printStatus(": onResume");
     }
 
     @Override
     protected void onPause(){
         super.onPause();
+        printStatus(": onPause");
     }
 
     @Override
     protected void onStop(){
         super.onStop();
+        printStatus(": onStop");
     }
 
     @Override
     protected void onRestart(){
         super.onRestart();
+        printStatus(": onRestart");
     }
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
+        printStatus(": onDestroy");
     }
 
 }
